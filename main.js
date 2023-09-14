8b5663e061e80e6516d0b0be5e35729d2d9203c4
Emma Grey <emgrey02@gmail.com>
2023-09-14 18:08:59 -0400

    Add new projects and view separate list for each

--- main.js
+++ main.js
@@ -116,62 +116,123 @@ const UIManager = (() => {
     let newProj = ProjectManager.createProject();
     ProjectManager.addProject(newProj);
 
-    //populate projects
+    // show appropriate todo items based on the project
+    const showProjTodos = (projIndex) => {
+        theList.setAttribute('data-projectId', projIndex);
+        populateTodos();
+    };
+
+    //clear project list
+    const clearProjectList = () => {
+        let lowerNav = document.querySelector('ul.lower-nav');
+
+        while (lowerNav.childElementCount != 1) {
+            lowerNav.lastChild.remove();
+        }
+    };
+
+    //populate project list
     const populateProjectList = () => {
         let lowerNav = document.querySelector('ul.lower-nav');
 
         let projects = ProjectManager.getAllProjects();
 
+        clearProjectList();
+
         for (let i = 0; i < projects.length; i++) {
             let listElement = document.createElement('li');
-            listElement.setAttribute('data-index', i);
+            listElement.setAttribute('data-id', i);
             let btnElement = document.createElement('button');
             listElement.appendChild(btnElement);
             btnElement.innerText = projects[i].name;
             lowerNav.appendChild(listElement);
         }
+
+        // add event listeners to buttons
+        let projBtns = document.querySelectorAll(`li[data-id] button`);
+        projBtns.forEach((btn) =>
+            btn.addEventListener('click', (e) => {
+                showProjTodos(+e.target.parentElement.getAttribute('data-id'));
+            }),
+        );
     };
 
+    //initial population
     populateProjectList();
 
+    // DOM objects
+
+    // add Task objects
     const addTaskBtn = document.querySelector('.add-task > button');
     const addTaskArea = document.querySelector('main .edit-area');
     const exitAddTaskArea = document.querySelector('button.exit');
     const addTaskForm = document.querySelector('.edit-area form');
+    const taskTitle = document.querySelector('.edit-area .text-ctn input');
+
+    // to-do list objects
     const theList = document.querySelector('div.the-list');
     const itemCont = document.querySelector('ul.ctn');
 
+    // add project objects
+    const newProjectBtn = document.querySelector(`div.lists-title button`);
+    const addProjectBtn = document.querySelector(
+        `div.btns button[type='submit']`,
+    );
+    const cancelAddProjectBtn = document.querySelector(
+        `div.btns button[type='button']`,
+    );
+    console.log(cancelAddProjectBtn);
+
+    // clear all todos //
+    // except the first one which is display: none and used for cloning //
     const clearTodos = () => {
         while (itemCont.childElementCount != 1) {
             itemCont.lastChild.remove();
         }
     };
 
+    // removes todo from DOM and from Project Object
     const removeTodo = (target) => {
         let todoIndex =
             target.parentElement.parentElement.getAttribute('data-index');
-        //make a copy...
+        // make a copy with slice - don't want to alter actual todos
         let todos = getTodos().slice(0);
+        // get the todo object from its index
         let todo = todos.filter((item) => item.index == todoIndex);
+        // remove it
         TodoManager.removeTodoFromProject(todo[0]);
+        // repopulate DOM
         populateTodos();
     };
 
+    // gets a list of todos from the current project
+    // we get the current project from a data attribute in the html
+    // we set this data attribute when we click a project in the nav
     const getTodos = () => {
         let projectId = theList.getAttribute('data-projectId');
-        return ProjectManager.getProject(projectId).todoList;
+        console.log('in getTodos:', projectId);
+        return ProjectManager.getProject(projectId).todoList || null;
     };
 
+    // populates DOM with todo list based on project
     const populateTodos = () => {
         const listItem = document.querySelector('li.list-item');
-        let todos = getTodos().slice(0);
+        let todos = getTodos()?.slice(0);
+
+        // first, clear the DOM
         clearTodos();
 
+        // repopulate DOM
         todos.forEach((item, index) => {
+            // use what's already in html
             let clone = listItem.cloneNode(true);
             itemCont.appendChild(clone);
             clone.style.display = 'grid';
+
+            // this helps us connect DOM item to todo item
             clone.setAttribute('data-index', index);
+
+            // input data into correct html elements
             document.querySelector(
                 `li[data-index='${index}'] .text-ctn .title`,
             ).innerText = item.title;
@@ -197,24 +258,33 @@ const UIManager = (() => {
         console.log(getTodos());
     };
 
+    // initial population of todo items
     populateTodos();
 
+    // called when we click the plus button to add a task
+    // opens the form to add a task and puts focus on the first input element
     const openAddTaskArea = () => {
         addTaskArea.style.visibility = 'initial';
         addTaskBtn.parentElement.style.display = 'none';
+        taskTitle.focus();
     };
 
+    // closes the form when exited or when submitted
     const closeAddTaskArea = () => {
         addTaskArea.style.visibility = 'hidden';
         addTaskBtn.parentElement.style.display = 'flex';
     };
 
+    // creates a todo object from entered form data
     const submitTask = () => {
+        // get form values
         const formData = new FormData(addTaskForm);
         let valueArray = [];
         for (const value of formData.values()) {
             valueArray.push(value);
         }
+
+        // create todo object
         let newTodo = TodoManager.createTodo(
             valueArray[0],
             valueArray[1],
@@ -222,26 +292,65 @@ const UIManager = (() => {
             valueArray[3],
             +theList.getAttribute('data-projectId'),
         );
+
+        // add todo object to current project
         TodoManager.addTodoToProject(newTodo);
+
+        //re-populate list with new todo added
         populateTodos();
+
+        // reset form values
+        addTaskForm.reset();
+
+        //close form
         closeAddTaskArea();
     };
 
-    //add a task
-    addTaskBtn.addEventListener('click', () => {
-        openAddTaskArea();
-    });
+    // opens area to add a new project
+    const openAddProjectArea = () => {
+        let addProjArea = document.querySelector('div.new-proj');
+        addProjArea.style.display = 'block';
+    };
 
-    exitAddTaskArea.addEventListener('click', () => {
-        closeAddTaskArea();
-    });
+    const closeAddProjectArea = () => {
+        let addProjArea = document.querySelector('div.new-proj');
+        addProjArea.style.display = 'none';
+    };
+
+    const submitNewProject = () => {
+        let form = document.querySelector('div.new-proj form');
+
+        let projName = document.querySelector(
+            'div.new-proj #project-title',
+        ).value;
+
+        let newProj = ProjectManager.createProject(projName);
+        ProjectManager.addProject(newProj);
+
+        populateProjectList();
+
+        form.reset();
+
+        closeAddProjectArea();
+    };
+
+    // event listeners
+    addTaskBtn.addEventListener('click', openAddTaskArea);
+
+    exitAddTaskArea.addEventListener('click', closeAddTaskArea);
 
     addTaskForm.addEventListener('submit', (e) => {
         e.preventDefault();
         submitTask();
     });
 
-    //delete a task
+    newProjectBtn.addEventListener('click', openAddProjectArea);
+
+    cancelAddProjectBtn.addEventListener('click', closeAddProjectArea);
+    addProjectBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        submitNewProject();
+    });
 })();
 
 //tests
